12f342495ba433b49d2ed82fe67d93f08d20a083
alvarouc <alvarouc@gmail.com>
2015-07-14 01:57:19 -0600

    updated demo and ipython notebook demo

--- demo.py
+++ demo.py
@@ -4,9 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Define matrix dimensions
-Nobs = 100 # Number of observations
-Nvars = 10000 # Number of variables
-Ncomp = 2 # Number of components
+Nobs = 1000 # Number of observations
+Nvars = 50000 # Number of variables
+Ncomp = 100 # Number of components
 
 # Simulated true sources
 S_true = np.random.logistic(0,1,(Ncomp,Nvars))
@@ -15,9 +15,10 @@ A_true = np.random.normal(0,1,(Nobs,Ncomp))
 # X = AS
 X = np.dot(A_true,S_true)
 # add some noise
-X = X + np.random.normal(0,5,X.shape)
+X = X + np.random.normal(0,1,X.shape)
 # apply ICA on X and ask for 2 components
-A,S = ica1(X,2)
+
+A,S = ica1(X,Ncomp)
 # compare if our estimates are accurate
 # correlate A with Atrue and take 
 aCorr = np.abs(np.corrcoef(A.T,A_true.T)[:Ncomp,Ncomp:]).max(axis = 0).mean()
