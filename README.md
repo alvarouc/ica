1d4e45aaa5142f9bca63496d08c41d88a5ab34c6
Alvaro Ulloa <alvarouc@gmail.com>
2016-07-07 13:58:12 -0600

    added installation steps in readme

--- README.md
+++ README.md
@@ -4,6 +4,15 @@ Python version of INFOMAX Independent component
 analysis. Ported from
 http://sccn.ucsd.edu/~scott/ica.html
 
+## Installation
+    pip install ica
+
+## How to use
+   from ica import ica1
+   A,S,W = ica1(X, n_components)
+
+Where, ica1 is the infomax ICA in function format. The input `X` is a numpy array and `n_components` is the number of components to estimate
+
 See use example in <a href="ICA-DEMO.ipynb">here</a>
 
 Minimum Requirements
--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name='ica',
     packages=['ica'],
     description='Independent component analysis with INFOMAX algorithm',
-    version='0.4',
+    version='0.5',
     url='https://github.com/alvarouc/ica/',
     author='Alvaro Ulloa',
     author_email='alvarouc@gmail.com',
-    download_url='https://github.com/alvarouc/ica/tarball/0.4',
+    download_url='https://github.com/alvarouc/ica/tarball/0.5',
     keywords=['infomax', 'ica', 'independent component analysis'],
     install_requires=[
         'numpy',
