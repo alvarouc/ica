46baa6f51e7eb66efe0e64fae1a8151f88513515
Alvaro Ulloa <alvarouc@gmail.com>
2016-07-07 13:05:31 -0600

    added setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='ica-mrn',
+    packages=['ica-mrn'],
+    description='Independent component analysis with INFOMAX algorithm',
+    version='0.1',
+    url='https://github.com/alvarouc/ica/',
+    author='Alvaro Ulloa',
+    author_email='alvarouc@gmail.com',
+    download_url='https://github.com/alvarouc/ica/tarball/0.1',
+    keywords=['infomax', 'ica', 'independent component analysis'],
+)
