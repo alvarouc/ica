e2afa233b41f5bdd3cc2f5d9d15f269c4926dacb
Alvaro Ulloa <alvarouc@gmail.com>
2016-07-07 13:39:45 -0600

    added requirements

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ setup(
     name='ica',
     packages=['ica'],
     description='Independent component analysis with INFOMAX algorithm',
-    version='0.1',
+    version='0.4',
     url='https://github.com/alvarouc/ica/',
     author='Alvaro Ulloa',
     author_email='alvarouc@gmail.com',
-    download_url='https://github.com/alvarouc/ica/tarball/0.1',
+    download_url='https://github.com/alvarouc/ica/tarball/0.4',
     keywords=['infomax', 'ica', 'independent component analysis'],
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
 )
